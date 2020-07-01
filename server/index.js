7acfe70fec31e97b1f0858597bdee536390fceb8
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-07-01 21:35:41 +0700

    add server-side portion

---
+++ server/index.js
@@ -0,0 +1,264 @@
+const winston = require('winston');
+const crypto = require('crypto');
+
+// database
+const db = require('../../../db.js');
+
+// room creation functs
+const Room = require('./room');
+
+///////////////////////////////////////////////////
+
+function colourFromUserId(userId) {
+    // Algorithm for converting between HSV taken from Wikipedia:
+    // https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+
+    let h = userId % 360;
+    let s = 1;
+    let v = 0.5;
+
+    let c = v * s;
+    let hPr = h / 60;
+    let x = c * (1 - Math.abs(hPr % 2 - 1));
+
+    let rgb1;
+    if (hPr <= 1) {
+        rgb1 = [c, x, 0];
+    } else if (hPr <= 2) {
+        rgb1 = [x, c, 0];
+    } else if (hPr <= 3) {
+        rgb1 = [0, c, x];
+    } else if (hPr <= 4) {
+        rgb1 = [0, x, c];
+    } else if (hPr <= 5) {
+        rgb1 = [x, 0, c];
+    } else {
+        rgb1 = [c, 0, x];
+    }
+
+    let m = v - c;
+    let rgb = rgb1.map(y => {
+        return Math.round((y + m) * 255);
+    });
+    console.log(rgb);
+
+    let colBuf = Buffer.allocUnsafe(4);
+    colBuf.writeUInt8(0xFF);
+    Buffer.from(rgb).copy(colBuf, 1);
+
+
+    return colBuf.readUInt32BE(0);
+}
+
+///////////////////////////////////////////////////
+
+const States = {
+    idle: 0,
+    lobby: 1,
+    game: 2
+}
+
+class Game {
+    // public:
+    //  (none)
+    // private:
+    #ws;            // web socket
+    #username;
+    #userId;
+    #sessionId;
+    #state;
+    #room;
+    #color;
+
+    constructor(ws, username, userId) {
+        this.#ws = ws;              // web socket object
+        this.#username = username;
+        this.#userId = userId;
+        this.#state = States.idle;  // default state
+        this.#room = null;          // default room
+
+        // set session ID
+        this.#sessionId = Math.floor(Math.random() * 1000000);
+
+        // generate a random color based on the user ID
+        this.#color = colourFromUserId(userId);
+
+        // close / crash message
+        ws.on("close", (code, reason) => {
+            winston.log('silly', `Entertaining Tiles client closed, code ${code}, reason: ${reason}`);
+        });
+
+        // install command handlers
+        ws.on("jsonMessage", this.handleMessage.bind(this));
+
+        // initialized
+        winston.log('verbose', `Entertaining Tiles client initialized for user ${username} (${userId})`);
+
+        // submit session ID
+        setTimeout(() => {
+            this.#ws.sendObject({
+                type: "sessionIdChanged",
+                session: this.#sessionId
+            });
+        }, 1000);
+    }
+
+    // message handler
+
+    async handleMessage(message) {
+        //Don't handle system messages
+        if (message.system) return;
+
+        // examine received message
+        console.log(message);
+        console.log(message.type);
+
+        // Bind the functions
+        const handlers = {
+            "openRoom": this.openRoom.bind(this),
+            "joinRoom": this.joinRoom.bind(this),
+            "quitRoom": this.quitRoom.bind(this),
+            "listRooms": this.listRooms.bind(this)
+        };
+
+        if (handlers.hasOwnProperty(message.type)) {
+            // on receive {"type": "whatever"}
+            // then call the function binded in handler["whatever"]
+            handlers[message.type](message);
+        } else if (this.#room) {
+            // if no "type" specified
+            this.#room.processMessage(this, message);
+        }
+    }
+
+    // bind functions
+
+    openRoom(){
+        if (this.#state != States.idle) {
+            this.#ws.close(1002); // we must be idle
+        } else {
+            let room = new Room();  // create a room
+            this.joinRoom({         // immediately join the room
+                "roomId": room.id
+            });
+        }
+    }
+
+    joinRoom(){
+        if (this.#state != States.idle) {
+            this.#ws.close(1002); // we must be idle
+        } else {
+            // join a room ID
+            let room = Room.roomById(message.roomId);
+
+            if (!room) {
+                // room not found
+                this.#ws.sendObject({
+                    type: "joinRoomFailed",
+                    reason: "room.invalid"
+                });
+                return;
+            }
+
+            // try to add user to the room
+            let addUserResponse = room.addUser(this);
+
+            if (addUserResponse !== "ok") {
+                this.#ws.sendObject({
+                    type: "joinRoomFailed",
+                    reason: addUserResponse
+                });
+                return;
+            }
+
+            // if we're successful, set the current user's room
+            this.#room = room;
+
+            this.stateChange("lobby") // user is now in the lobby, they
+                                      // can't open another room at the
+                                      // same time
+        }
+    }
+
+    quitRoom(){
+        if (this.#state == States.idle) {
+            this.#ws.close(1002); // we must NOT be in idle
+        } else {
+            this.#room.removeUser(this);
+            this.#room = null;
+
+            this.changeState("idle");
+        }
+    }
+
+    async listRooms(){
+        // initialize rooms list
+        let rooms = [];
+
+        // get all the user's friends
+        let friends = await db.friendsForUserId(this.#userId);
+
+        // should be a list, get all open rooms
+        for (let friend of friends) {
+            let rs = Room.roomsByUserId(friend.userId);
+            for (let room of rs) {
+                room.friendName = friend.username;
+                rooms.push(room);
+            }
+        }
+
+        // send off the room IDs
+        this.#ws.sendObject({
+            type: "availableRoomsReply",
+            rooms: rooms.map(room => {
+                return {
+                    friend: room.friendName,
+                    roomId: room.id,
+                    pinRequired: room.pinRequired
+                };
+            })
+        });
+    }
+
+    // general functions
+
+    changeState(state) {
+        this.#ws.sendObject({
+            type: "stateChange",
+            newState: state
+        });
+        this.#state = States[state];
+    }
+
+    // getters
+
+    static displayName() {
+        return "Entertaining Tiles";
+    }
+
+    get username() {
+        return this.#username;
+    }
+
+    get userId() {
+        return this.#userId;
+    }
+
+    get ws() {
+        return this.#ws;
+    }
+
+    get picture() {
+        return this.#picture;
+    }
+
+    get colour() {
+        return this.#colour;
+    }
+
+    get sessionId() {
+        return this.#sessionId;
+    }
+}
+
+module.exports = Game;
---
+++ server/room.js
@@ -0,0 +1,220 @@
+const winston = require('winston');
+
+// at start of server, we have an empty rooms list
+let rooms = {};
+
+class Room {
+    #id;
+    #users;
+    #playing;
+    #maxUsers;
+    #board;
+
+    // begin room
+
+    constructor() {
+        this.#id = Room.generateId();
+        this.#users = [];
+        this.#playing = false;
+        this.#maxUsers = 2;
+
+        // add a Room object at the generated key
+        rooms[this.#id] = this;
+
+        winston.log("verbose", `Entertaining Tiles room #${this.#id} created`);
+    }
+
+    static generateId() {
+        return Math.floor(Math.random() * 10000000 + 1000000);
+    }
+
+    // fetch rooms
+
+    static roomById(id) {
+        return rooms[id];
+    }
+
+    static roomsByUserId(userId) {
+        // Find all the rooms where this user is a member
+        let rs = Object.values(rooms).filter(room => {
+            for (let user of room.users) {
+                if (user.userId === userId) return true;
+            }
+            return false;
+        });
+
+        return rs;
+    }
+
+    // handler install
+
+    processMessage(user, message) {
+        // lobby-time messages
+        if (this.isHost(user)) {
+            const handlers = {
+                "startGame": this.startGame.bind(this)
+            };
+
+            // register handlers
+            if (handlers.hasOwnProperty(message.type)) {
+                handlers[message.type](user, message);
+            }
+        }
+
+        // game-time messages
+        const handlers = {
+            "currentTileChanged": this.currentTileChanged.bind(this),
+            //"sendMessage": this.sendCannedMessage.bind(this)
+        };
+        if (handlers.hasOwnProperty(message.type)) {
+            handlers[message.type](user, message);
+        }
+    }
+
+    // user functions
+
+    addUser(user) {
+        if (this.#playing) {
+            return "room.closed";
+        }
+        if (this.#users.count >= this.#maxUsers) {
+            return "room.full";
+        }
+
+        // add user to room users list
+        this.#users.push(user);
+
+        // remove on unexpected disconnect
+        user.ws.on("close", () => {
+            this.removeUser(user);
+        });
+
+        // let user client know the lobby is changed
+        user.ws.sendObject({
+            type: "lobbyChange",
+            lobbyId: this.#id
+        });
+
+        this.beamRoomUpdate();
+
+        return "ok";
+    }
+
+    removeUser(user) {
+        if (this.#users.includes(user)) {
+            this.#users.splice(this.#users.indexOf(user), 1);
+
+            if (this.#playing) {
+                this.#board.removeUser(user);
+            }
+
+            user.ws.sendObject({
+                type: "lobbyChange",
+                lobbyId: -1
+            });
+
+            if (this.#users.length == 0) {
+                deleteRoom();
+            } else {
+                this.beamRoomUpdate();
+            }
+        }
+    }
+
+    deleteRoom(){
+        //Close this room
+        delete rooms[this.#id];
+        winston.log("verbose", `Entertaining Tiles room #${this.#id} closed`);
+    }
+
+    // send to clients
+
+    beamRoomUpdate() {
+        // let everyone in the room know about changes in setup
+        let users = [];
+
+        // add every user we can find to the list
+        for (let user of this.#users) {
+            users.push({
+                session: user.sessionId,
+                username: user.username,
+                picture: user.picture,
+                colour: user.colour,
+                isHost: this.isHost(user)
+            });
+        }
+
+        // send list of users to every client
+        let message = {
+            type: "roomUpdate",
+            users: users,
+            maxUsers: this.#maxUsers
+        };
+        this.beam(message);
+    }
+
+    beam(message) {
+        // send a json object to all clients currently connected
+        for (let user of this.#users) {
+            this.sendMessage(user, message);
+        }
+    }
+
+    sendMessage(user, message) {
+        // send a json object to a specific user
+        user.ws.sendObject(message);
+    }
+
+    // game control
+
+    startGame(user, message) {
+        // Start the game state
+        this.#playing = true;
+
+        // everyone is now playing the game
+        for (let user of this.#users) {
+            user.changeState("game");
+        }
+    }
+
+    endGame() {
+        // everyone is off the game
+        for (let user of this.#users) {
+            user.changeState("lobby");
+        }
+
+        this.beamRoomUpdate();
+
+        // End the game state
+        this.#playing = false;
+    }
+
+    // signals
+
+    currentTileChanged(user, message) {
+        if (this.#playing) {
+            // TODO: make handler
+            console.log(user, message)
+        }
+    }
+
+    changeBoardParams(user, message) {
+        // TODO: make handler
+    };
+
+    // getters
+
+    isHost(user) {
+        return this.#users[0] === user;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get users() {
+        return this.#users;
+    }
+}
+
+module.exports = Room
